a102b60b92b83aa3d011707490a05e468448e076
ys008 <90621970+ys008@users.noreply.github.com>
2025-11-05 20:40:16 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 function createText() {
     const text = document.createElement('div');
     text.classList.add('text');
-    if (Math.random() > 0.5) {
-        text.textContent = 'しのぴこたろう';
-    } else {
-        text.textContent = 'わたあめ';
-        text.classList.add('wataame');
-    }
+    text.textContent = 'しのぴこたろう';
 
     // ランダムなプロパティを設定
     const startX = Math.random() * window.innerWidth;
@@ -25,5 +20,43 @@ function createText() {
     }, duration * 1000);
 }
 
-// 200ミリ秒ごとに新しいテキストを生成
-setInterval(createText, 200);
+function createWataame() {
+    const wataame = document.createElement('div');
+    wataame.classList.add('wataame-container');
+
+    const startX = Math.random() * window.innerWidth;
+    const duration = Math.random() * 5 + 4; // 4秒から9秒
+
+    wataame.style.left = `${startX}px`;
+    wataame.style.animationDuration = `${duration}s`;
+
+    document.body.appendChild(wataame);
+
+    setTimeout(() => {
+        wataame.remove();
+    }, duration * 1000);
+}
+
+function createCandy() {
+    const candy = document.createElement('div');
+    candy.classList.add('candy');
+
+    const startX = Math.random() * window.innerWidth;
+    const duration = Math.random() * 5 + 4; // 4秒から9秒
+
+    candy.style.left = `${startX}px`;
+    candy.style.animationDuration = `${duration}s`;
+
+    document.body.appendChild(candy);
+
+    setTimeout(() => {
+        candy.remove();
+    }, duration * 1000);
+}
+
+// 200ミリ秒ごとに新しいテキストとわたあめとキャンディを生成
+setInterval(() => {
+    createText();
+    createWataame();
+    createCandy();
+}, 200);
